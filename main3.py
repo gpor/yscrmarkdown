86fe8a277fdf1d2d706191ae0116941e24f76696
gpor <lee.m.stevens@gmail.com>
2025-09-21 12:37:42 +1000

    remove old scripts

--- main3.py
+++
@@ -1,9 +0,0 @@
-from lib.scrape import get_scrape_dir
-
-
-if __name__ == "__main__":
-    parent_dir, scrape_dir = get_scrape_dir("example")
-    print(f"parent_dir = {parent_dir}")
-    print(parent_dir.exists())
-    print(f"scrape_dir = {scrape_dir}")
-    print(scrape_dir.exists())
